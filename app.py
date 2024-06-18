400e14b65f9bddfe1499eb41dc7cec16b5c017d9
Darshan485213 <139923389+Darshan485213@users.noreply.github.com>
2024-06-18 19:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from realtime_data import main  
+from realtime_data import main
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/dashboard', methods=['GET'])
 def dashboard_data():
@@ -11,4 +15,4 @@ def dashboard_data():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
