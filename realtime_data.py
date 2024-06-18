ef2c2fb3af6178f30f6faae5d6b7f820768bef60
Darshan485213 <139923389+Darshan485213@users.noreply.github.com>
2024-06-18 19:22:51 +0530

    Update realtime_data.py

--- realtime_data.py
+++ realtime_data.py
@@ -1,10 +1,14 @@
+import os
 import requests
 import json
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 AUTH_URL = 'https://lb.solinteg-cloud.com/openapi/v2/loginv2/auth'
-AUTH_ACCOUNT = 'shailendra.nair@atriapower.com'
-AUTH_PASSWORD = 'SolarEnergy'
+AUTH_ACCOUNT = os.getenv('AUTH_ACCOUNT')
+AUTH_PASSWORD = os.getenv('AUTH_PASSWORD')
 BASE_DATA_URL = 'https://lb.solinteg-cloud.com/openapi/v2/device/queryDeviceRealtimeData'
 DAY_AGGREGATE_URL = 'https://lb.solinteg-cloud.com/openapi/v2/device/queryDayAggregateValues'
 MONTH_AGGREGATE_URL = 'https://lb.solinteg-cloud.com/openapi/v2/device/queryMonthAggregateValues'
