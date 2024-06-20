1283defc372816b823597ec0aea13ee05a3911aa
Darshan485213 <139923389+Darshan485213@users.noreply.github.com>
2024-06-20 17:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def dashboard_data():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5001))
